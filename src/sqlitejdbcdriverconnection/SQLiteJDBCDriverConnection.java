40a75ef06e1345c69be603e3f1f347da6d6249ed
ifernandezblanco <ifernandezblanco@nautilus29.danielcastelao.org>
2019-05-09 10:09:32 +0200

    Clase connect creada Mensaje ->Connection to SQLite has been established. Tabla WareHouse creada sin columnas Tabla warehouse con tres columnas nuevas

--- src/sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
+++ src/sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
@@ -2,6 +2,7 @@ package sqlitejdbcdriverconnection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -55,6 +56,44 @@ public class SQLiteJDBCDriverConnection {
             System.out.println(e.getMessage());
         }
     }
+        public static class InsertApp {
+ 
+    /**
+     * Connect to the test.db database
+     *
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:chinook.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+ 
+    /**
+     * Insert a new row into the warehouses table
+     *
+     * @param name
+     * @param capacity
+     */
+    public void insert(String name, double capacity) {
+        String sql = "INSERT INTO warehouses(name,capacity) VALUES(?,?)";
+ 
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+        }
     
     /**
      * @param args the command line arguments
@@ -63,6 +102,11 @@ public class SQLiteJDBCDriverConnection {
 public static void main(String[] args) {
         connect();
         createNewTable();
+        InsertApp app = new InsertApp();
+        // insert three new rows
+        app.insert("Raw Materials", 3000);
+        app.insert("Semifinished Goods", 4000);
+        app.insert("Finished Goods", 5000);
       
 } 
 }
