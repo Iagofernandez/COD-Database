c7bc4ee1d10e85fc7437866ff255a636d57bbca2
ifernandezblanco <ifernandezblanco@nautilus29.danielcastelao.org>
2019-05-09 10:15:11 +0200

    Clase connect creada Mensaje ->Connection to SQLite has been established. Tabla WareHouse creada sin columnas Tabla warehouse con tres columnas nuevas Columna 3 actualizada

--- src/sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
+++ src/sqlitejdbcdriverconnection/SQLiteJDBCDriverConnection.java
@@ -94,6 +94,51 @@ public class SQLiteJDBCDriverConnection {
         }
     }
         }
+        public static class UpdateApp {
+ 
+    /**
+     * Connect to the test.db database
+     *
+     * @return the Connection object
+     */
+    private Connection connect() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:C://sqlite/db/test.db";
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
+ 
+    /**
+     * Update data of a warehouse specified by the id
+     *
+     * @param id
+     * @param name name of the warehouse
+     * @param capacity capacity of the warehouse
+     */
+    public void update1(int id, String name, double capacity) {
+        String sql = "UPDATE warehouses SET name = ? , "
+                + "capacity = ? "
+                + "WHERE id = ?";
+ 
+        try (Connection conn = this.connect();
+                PreparedStatement pstmt = conn.prepareStatement(sql)) {
+ 
+            // set the corresponding param
+            pstmt.setString(1, name);
+            pstmt.setDouble(2, capacity);
+            pstmt.setInt(3, id);
+            // update 
+            pstmt.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+        }
     
     /**
      * @param args the command line arguments
@@ -102,11 +147,17 @@ public class SQLiteJDBCDriverConnection {
 public static void main(String[] args) {
         connect();
         createNewTable();
+        
+        
         InsertApp app = new InsertApp();
         // insert three new rows
         app.insert("Raw Materials", 3000);
         app.insert("Semifinished Goods", 4000);
         app.insert("Finished Goods", 5000);
+        
+        UpdateApp app1 = new UpdateApp();
+        // update the warehouse with id 3
+        app1.update1(3, "Finished Products", 5500);
       
 } 
 }
